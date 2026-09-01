a316bd000a9cd46a2fc6b45caf39c1415f31b64e
Xeomus <a180313@une.edu.mx>
2026-09-01 12:32:06 -0600

    fix: configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ejercicios-react/",
+  base: "/RickAndMortyAPI/",
   plugins: [react()],
 });
